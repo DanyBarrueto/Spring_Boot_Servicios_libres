cf00ef03b5e9e1f7c8f7050333ad7a933b4bccdd
DanyBarrueto <dfbarrueto@hotmail.com>
2024-08-18 17:40:16 -0500

    V1.2
    Configuración del controlador, cambio de nombre a 08-ServiciosLibres (Funciona Completamente)

--- src/main/java/com/edu/ue/client/myfifthproyect/controller/MunicipioController.java
+++ src/main/java/com/edu/ue/client/myfifthproyect/controller/MunicipioController.java
@@ -1,9 +1,32 @@
 package com.edu.ue.client.myfifthproyect.controller;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.edu.ue.client.myfifthproyect.model.Municipio;
+import com.edu.ue.client.myfifthproyect.service.MunicipioServiceI;
+
+@RestController
 public class MunicipioController {
 
+    @Autowired
+    MunicipioServiceI service;
 
+    @GetMapping(value = "municipios", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Municipio> municipios(){    
+
+        return service.listMunicipios();
+    }
+
+    @GetMapping(value = "municipios/{departamento}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Municipio> municipios(@PathVariable("departamento") String depart){
+
+        return service.searchDepartamento(depart);
+    }
 
 }
